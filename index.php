bd6eaa329d1f395e19af6eef90d616c290cd7e5a
Joel Thoman <jethoman@gmail.com>
2017-02-04 18:00:55 -0500

    preparing files for homework

---
+++ index.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Some Book Suggestions from David</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="books.css">
+</head>
